92273e7a40a04d9c410c6a703756a50adc17bde0
Nicolás Giménez <nicolasg143@hotmail.com>
2022-11-17 21:47:04 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ import { WORDS } from './words.js'
 
 let game = document.querySelector(".container-word")
 let charButtons = document.querySelector(".container-buttons-chars")
-
-function randomNumber(){
-    return Math.floor(Math.random() * WORDS.length)
-}
+let image = document.querySelector(".image")
+let tries = 0
+let pressedKeys = []
+let validKeys = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
 let palabra = WORDS[randomNumber()]
 
@@ -19,6 +19,8 @@ for (const i of palabra) {
     game.innerHTML += char
 }
 
+palabra = palabra.join('')
+
 for(let i = 65; i<=90;i++){
     charButtons.innerHTML += `<div class="char-button">${String.fromCharCode(i)}</div>`
     if (i == 78){
@@ -26,4 +28,40 @@ for(let i = 65; i<=90;i++){
     }
 }
 
-console.log(palabra)
+function randomNumber(){
+    return Math.floor(Math.random() * WORDS.length)
+}
+
+function checkLetters(char){
+        for(let i = 0;i<= palabra.length;i++){
+            if (!palabra.includes(char)){
+                changeImage()
+                break
+            }
+            if (palabra[i] === char){
+                console.log(game.children[i]);
+                game.children[i].innerHTML = char
+            }
+        }
+}
+
+function changeImage(){
+    if (tries < 6){
+        tries += 1
+        console.log(tries)
+        image.setAttribute('src', `./imgs/${tries}.png`)
+    }
+}
+
+
+
+document.addEventListener('keypress', (e) => {
+    let letra = e.key.toUpperCase()
+    if (validKeys.includes(letra)){
+        if (!pressedKeys.includes(letra)){
+            checkLetters(letra);
+            pressedKeys.push(letra)
+            console.log(letra)
+        }
+    }
+})
